c240946ecb7c40722e220affa0cb25f2433a9fd0
Komilxon <komilkhon.asamov@hotmail.com>
2016-05-30 14:58:46 +0500

    Show furniture from model in index page

--- face/views.py
+++ face/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Category
+from .models import Category, FurnitureItem
 
 
 def get_index_page(request):
     return render(request, 'face/index.html', {
         'categories': Category.objects.all(),
+        'items': FurnitureItem.objects.all(),
     })
