ee96c5806181c66b1b3b36f7a3c27fd9005b7213
Komilxon <komilkhon.asamov@hotmail.com>
2016-05-30 13:59:56 +0500

    Add model Category
    It is linked to admin panel

--- face/admin.py
+++ face/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,
+            {'fields':
+                ['name', 'tech_name']
+            }
+        )
+    ]
+    list_display = ('name',)
