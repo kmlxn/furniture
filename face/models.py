74d315847a08ba150a7b1991202acfc7eb052080
Komilxon <komilkhon.asamov@hotmail.com>
2016-05-30 14:49:13 +0500

    Add __str__ method to models

--- face/models.py
+++ face/models.py
@@ -5,8 +5,13 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     tech_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
 
 class FurnitureItem(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField()
     category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.name
